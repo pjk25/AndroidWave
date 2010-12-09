32c78d4d3b70d3eeeb0e3db2c241df36f1098e05
Philip Kuryloski <pjk25@cornell.edu>
2010-12-09 12:54:03 -0800

    Initial commit of empty CalFitWave project

---
+++ src/edu/berkeley/calfitwave/CalFitWaveActivity.java
@@ -0,0 +1,15 @@
+package edu.berkeley.calfitwave;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalFitWaveActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
