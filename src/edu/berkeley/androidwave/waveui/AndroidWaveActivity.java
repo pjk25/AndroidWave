0d45df247eaad5b198a2d2b01813dc2b396f940f
Philip Kuryloski <pjk25@cornell.edu>
2011-04-06 22:48:23 -0700

    AndroidWaveActivity now connects to WaveService

--- src/edu/berkeley/androidwave/waveui/AndroidWaveActivity.java
+++ src/edu/berkeley/androidwave/waveui/AndroidWaveActivity.java
@@ -1,8 +1,16 @@
 package edu.berkeley.androidwave.waveui;
 
-import android.app.Activity;
-import android.os.Bundle;
 import edu.berkeley.androidwave.R;
+import edu.berkeley.androidwave.waveservice.WaveService;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
 
 /**
  * AndroidWaveActivity
@@ -11,8 +19,11 @@ import edu.berkeley.androidwave.R;
  * displayed when the user launches the Wave app on their phone. It should
  * bind to the WaveService over the private local binding.
  */
-public class AndroidWaveActivity extends Activity
-{
+public class AndroidWaveActivity extends Activity {
+
+    protected WaveService mService;
+    protected boolean mBound = false;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -20,5 +31,43 @@ public class AndroidWaveActivity extends Activity
         super.onCreate(savedInstanceState);
         setTitle(R.string.main_activity_title);
         setContentView(R.layout.main);
+
+        // connect to WaveService
+        Intent sIntent = new Intent(Intent.ACTION_MAIN);
+        sIntent.setClass(this, WaveService.class);
+        try {
+            bindService(sIntent, mConnection, Context.BIND_AUTO_CREATE);
+        } catch (Exception e) {
+            Toast.makeText(AndroidWaveActivity.this, "Exception encountered, see log.", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            // setResult(RESULT_CANCELED);
+            // finish();
+        }
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+    
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className,
+                IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            mService = ((WaveService.LocalBinder)service).getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            mBound = false;
+        }
+    };
 }
