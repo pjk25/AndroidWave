a7e2183b138ae26f5f0f02309e5a7a43de9ffdec
Philip Kuryloski <pjk25@cornell.edu>
2011-04-18 17:45:38 -0700

    Minimal tests added to AndroidWaveSensorTest
    We test functionality as it is conceived thus far

--- tests/src/edu/berkeley/androidwave/waveservice/sensorengine/AndroidWaveSensorTest.java
+++ tests/src/edu/berkeley/androidwave/waveservice/sensorengine/AndroidWaveSensorTest.java
@@ -8,6 +8,10 @@
 
 package edu.berkeley.androidwave.waveservice.sensorengine;
 
+import edu.berkeley.androidwave.TestUtils;
+import edu.berkeley.androidwave.waverecipe.WaveSensorDescription;
+
+import android.hardware.Sensor;
 import android.test.AndroidTestCase;
 import android.test.MoreAsserts;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -16,10 +20,38 @@ import android.test.suitebuilder.annotation.SmallTest;
 
 /**
  * AndroidWaveSensorTest
+ * 
+ * @see AndroidWaveSensor
+ * 
+ * To run this test, you can type:
+ * adb shell am instrument -w -e class edu.berkeley.androidwave.waveservice.sensorengine.AndroidWaveSensorTest edu.berkeley.androidwave.tests/android.test.InstrumentationTestRunner
  */
 public class AndroidWaveSensorTest extends AndroidTestCase {
     
-    public void testSomething() {
-        fail("test not written yet");
+    AndroidWaveSensor aws;
+    
+    @Override
+    public void setUp() {
+        aws = new AndroidWaveSensor(WaveSensorDescription.Type.ACCELEROMETER, "-m/s^2");
+    }
+    
+    public void tesGetVersion() {
+        String methodSig = TestUtils.methodSignature(aws,
+                                                     "public",
+                                                     false,
+                                                     String.class,
+                                                     "getVersion",
+                                                     new Class[0]);
+        TestUtils.assertHasMethod(methodSig, true, aws);
+    }
+    
+    public void testGetAndroidSensor() {
+        String methodSig = TestUtils.methodSignature(aws,
+                                                     "public",
+                                                     false,
+                                                     Sensor.class,
+                                                     "getAndroidSensor",
+                                                     new Class[0]);
+        TestUtils.assertHasMethod(methodSig, true, aws);
     }
 }
