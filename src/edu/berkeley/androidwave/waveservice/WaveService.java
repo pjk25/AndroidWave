pjk25/AndroidWave

commit 861f4a7dee979174d70b609f004d33dd7a7d56e8

Authorizations can now be revoked!
Required reworking of database storage, so
previous test databases are invalidated (I didn't
change the database version or write an update
method).

Also changed tests so that the AndroidWaveRecipe
is not removed from the on-disk cache.

src/edu/berkeley/androidwave/waveservice/WaveService.java
@@ -46,10 +46,10 @@ public class WaveService extends Service {
     
     protected RecipeDbHelper databaseHelper;
     
+    // Name in these maps refers to the client's package name
     protected Map<String, String> clientKeyNameMap;
+    protected Map<String, String> clientNameKeyMap;
     protected ArrayList<WaveRecipeAuthorization> authorizations;
-    protected Map<String, Set<WaveRecipeAuthorization> > validAuthorizationsByClientKey;
-    protected Map<String, Set<WaveRecipeAuthorization> > revokedAuthorizationsByClientKey;
     
     protected void throwNotImplemented() {
         String className = getClass().getName();
@@ -67,35 +67,14 @@ public class WaveService extends Service {
              */
             databaseHelper = new RecipeDbHelper(this);
         
+            // load the client auth map (and create the inverse map)
             clientKeyNameMap = databaseHelper.loadClientKeyNameMap();
-            // invert the map for this portion
-            HashMap<String, String> clientNameKeyMap = new HashMap<String, String>();
+            clientNameKeyMap = new HashMap<String, String>();
             for (Map.Entry<String, String> entry : clientKeyNameMap.entrySet()) {
                 clientNameKeyMap.put(entry.getValue(), entry.getKey());
             }
         
-            authorizations = new ArrayList<WaveRecipeAuthorization>();
-        
-            ArrayList<WaveRecipeAuthorization> validAuthorizations = databaseHelper.loadAuthorized(this); // the waveservice is passed so that we can load recipes
-            authorizations.addAll(validAuthorizations);
-        
-            // sort the authorizations by client name
-            // maybe we should have a different table for each clientName?
-            validAuthorizationsByClientKey = new HashMap<String, Set<WaveRecipeAuthorization> >();
-            for (WaveRecipeAuthorization auth : validAuthorizations) {
-                String clientName = auth.getRecipeClientName().getPackageName();
-                if (clientNameKeyMap.containsKey(clientName)) {
-                    String clientKey = clientNameKeyMap.get(clientName);
-                    if (!validAuthorizationsByClientKey.containsKey(clientKey)) {
-                        validAuthorizationsByClientKey.put(clientKey, new HashSet<WaveRecipeAuthorization>());
-                    }
-                    Set<WaveRecipeAuthorization> auths = validAuthorizationsByClientKey.get(clientKey);
-                    auths.add(auth);
-                } else {
-                    Log.d(TAG, "Could not find key for recipe client with name "+clientName+" skipping stored auth "+auth);
-                }
-            }
-            // for now we will ignore the revoked auths
+            authorizations = databaseHelper.loadAuthorizations(this); // the waveservice is passed so that we can load recipes
         } catch (Exception e) {
             Log.d(TAG, "Exception encountered in onCreate()", e);
         }
@@ -236,28 +215,18 @@ public class WaveService extends Service {
     /**
      * saveAuthorization
      * 
-     * save a new authorization.  Note that we don't check the validity of the
-     * client key, because this is a private method.
+     * save an authorization.  Note that we don't check the validity of the
+     * client key, because this is a private interface method.
      */
-    public synchronized boolean saveAuthorization(String clientKey, WaveRecipeAuthorization auth) {
-        if (databaseHelper.saveAuthorization(auth)) {
-            authorizations.add(auth);
-            
-            if (!validAuthorizationsByClientKey.containsKey(clientKey)) {
-                validAuthorizationsByClientKey.put(clientKey, new HashSet<WaveRecipeAuthorization>());
+    public synchronized boolean saveAuthorization(WaveRecipeAuthorization auth) {
+        try {
+            if (databaseHelper.insertOrUpdateAuthorization(auth)) {
+                authorizations.add(auth);
+                return true;
             }
-            Set<WaveRecipeAuthorization> auths = validAuthorizationsByClientKey.get(clientKey);
-            auths.add(auth);
-            return true;
+        } catch (Exception e) {
+            Log.w(TAG, "Exception raised while saving authorization", e);
         }
-        return false;
-    }
-    
-    /**
-     * updateAuthorization
-     */
-    public synchronized boolean updateAuthorization(WaveRecipeAuthorization auth) {
-        // null implementation
         return false;
     }
     
@@ -268,7 +237,6 @@ public class WaveService extends Service {
      * object
      */
     public synchronized WaveRecipeAuthorization getAuthorization(String recipeId, ComponentName clientName) {
-        Date now = new Date();
         // first look for a full name match
         for (WaveRecipeAuthorization auth : authorizations) {
             if (auth.getRecipe().getId().equals(recipeId)) {
@@ -277,7 +245,21 @@ public class WaveService extends Service {
                 }
             }
         }
-        // TODO: then just a package match
+        // then look for a package match
+        return getAuthorization(recipeId, clientName.getPackageName());
+    }
+    
+    /**
+     * alternate getAuthorization matching on recipeId and client package name
+     */
+    protected WaveRecipeAuthorization getAuthorization(String recipeId, String clientPackageName) {
+        for (WaveRecipeAuthorization auth : authorizations) {
+            if (auth.getRecipe().getId().equals(recipeId)) {
+                if (clientPackageName.equals(auth.getRecipeClientName().getPackageName())) {
+                    return auth;
+                }
+            }
+        }
         return null;
     }
     
@@ -291,9 +273,8 @@ public class WaveService extends Service {
         databaseHelper.emptyDatabase();
         
         clientKeyNameMap = new HashMap<String, String>();
+        clientNameKeyMap = new HashMap<String, String>();
         authorizations = new ArrayList<WaveRecipeAuthorization>();
-        validAuthorizationsByClientKey = new HashMap<String, Set<WaveRecipeAuthorization> >();
-        revokedAuthorizationsByClientKey = new HashMap<String, Set<WaveRecipeAuthorization> >();
         
         Log.d(TAG, "end resetDatabase()");
     }
@@ -303,67 +284,71 @@ public class WaveService extends Service {
      * 
      * @see IWaveServicePublic
      */
-     private final IWaveServicePublic.Stub mPublicBinder = new IWaveServicePublic.Stub() {
-         
-         private WaveRecipeAuthorization retrieveAuthorization(String key, String recipeId) {
-             if (key != null && validAuthorizationsByClientKey.containsKey(key)) {
-                // we recognize the client
-                // now search for the specific auth
-                Set<WaveRecipeAuthorization> auths = validAuthorizationsByClientKey.get(key);
-                for (WaveRecipeAuthorization anAuth : auths) {
-                    if (recipeId.equals(anAuth.getRecipe().getId())) {
-                        return anAuth;
-                    }
+    private final IWaveServicePublic.Stub mPublicBinder = new IWaveServicePublic.Stub() {
+        
+        private WaveRecipeAuthorization retrieveAuthorization(String key, String recipeId) {
+            // check the validity of the key
+            if (clientKeyNameMap.containsKey(key)) {
+                // recall the package name for the given key
+                // this is a valid key
+                String packageName = clientKeyNameMap.get(key);
+                WaveRecipeAuthorization auth = getAuthorization(recipeId, packageName);
+                if (auth.validForDate(new Date())) {
+                    return auth;
+                } else {
+                    return null;
                 }
-             }
-             return null;
-         }
-         
-         /**
-          * isAuthorized
-          */
-         public boolean isAuthorized(String key, String recipeId) {
-             WaveRecipeAuthorization auth = retrieveAuthorization(key, recipeId);
-             return (auth != null);
-         }
+            }
+            // invalid key, return null
+            Log.d(TAG, "retrieveAuthorization called using invalid key (for recipeId="+recipeId+")");
+            return null;
+        }
 
-         /**
-          * retrieveAuthorization
-          */
-         public WaveRecipeAuthorizationInfo retrieveAuthorizationInfo(String key, String recipeId) {
-             WaveRecipeAuthorization auth = retrieveAuthorization(key, recipeId);
-             if (auth != null) {
+        /**
+         * isAuthorized
+         */
+        public boolean isAuthorized(String key, String recipeId) {
+            WaveRecipeAuthorization auth = retrieveAuthorization(key, recipeId);
+            return (auth != null);
+        }
+
+        /**
+         * retrieveAuthorization
+         */
+        public WaveRecipeAuthorizationInfo retrieveAuthorizationInfo(String key, String recipeId) {
+            WaveRecipeAuthorization auth = retrieveAuthorization(key, recipeId);
+            if (auth != null) {
                 return auth.asInfo();
-             }
-             return null;
-         }
-         
-         /**
-          * getAuthorizationIntent
-          *
-          * TODO: avoid the use of the key, and determine the caller of the IPC dynamically
-          */
-         public Intent getAuthorizationIntent(String recipeId, String key) {
-             Intent authIntent = new Intent(ACTION_REQUEST_RECIPE_AUTHORIZE);
-             authIntent.putExtra(RECIPE_ID_EXTRA, recipeId);
-             authIntent.putExtra(CLIENT_KEY_EXTRA, key);
-             return authIntent;
-         }
+            }
+            return null;
+        }
 
-         /**
-          * registerRecipeOutputListener
-          */
-         public boolean registerRecipeOutputListener(String key, String recipeId, IWaveRecipeOutputDataListener listener) {
-             throwNotImplemented();
-             return false;
-         }
-         
-         /**
-          * unregisterRecipeOutputListener
-          */
-         public boolean unregisterRecipeOutputListener(String key, String recipeId, IWaveRecipeOutputDataListener listener) {
-             throwNotImplemented();
-             return false;
-         }
-     };
+        /**
+         * getAuthorizationIntent
+         *
+         * TODO: avoid the use of the key, and determine the caller of the IPC dynamically
+         */
+        public Intent getAuthorizationIntent(String recipeId, String key) {
+            Intent authIntent = new Intent(ACTION_REQUEST_RECIPE_AUTHORIZE);
+            authIntent.putExtra(RECIPE_ID_EXTRA, recipeId);
+            authIntent.putExtra(CLIENT_KEY_EXTRA, key);
+            return authIntent;
+        }
+
+        /**
+         * registerRecipeOutputListener
+         */
+        public boolean registerRecipeOutputListener(String key, String recipeId, IWaveRecipeOutputDataListener listener) {
+            throwNotImplemented();
+            return false;
+        }
+
+        /**
+        * unregisterRecipeOutputListener
+        */
+        public boolean unregisterRecipeOutputListener(String key, String recipeId, IWaveRecipeOutputDataListener listener) {
+            throwNotImplemented();
+            return false;
+        }
+    };
 }

res/values/strings.xml
@@ -9,9 +9,6 @@
     
     <!-- AndroidWaveActivity strings -->
     <string name="androidwave_welcome">
-        Welcome to the AndroidWave Manager.\n\nYour recipes are loading...
-    </string>
-    <string name="androidwave_no_recipes">
         Welcome to the AndroidWave Manager.\n\nYou currently have not yet
         authorized any recipes.  Any recipes that have been authorized will
         appear here.

src/edu/berkeley/androidwave/waveservice/RecipeDbHelper.java
@@ -41,9 +41,9 @@ public class RecipeDbHelper {
         public static final String _ID = "_id";
         // public static final String _COUNT = "_count";
         public static final String CLIENT_KEY = "client_key";
-        public static final String CLIENT_NAME = "client_name";
+        public static final String CLIENT_PKG = "client_pkg";
         
-        public static final String[] ALL = {CLIENT_KEY, CLIENT_NAME};
+        public static final String[] ALL = {CLIENT_KEY, CLIENT_PKG};
     }
     
     static final class AuthColumns {
@@ -51,14 +51,11 @@ public class RecipeDbHelper {
         // public static final String _COUNT = "_count";
         public static final String RECIPE_ID = "recipe_id";
         public static final String SIGNATURE = "signature";
-        public static final String AUTH_TS = "authorized";
-        public static final String REVOKED_TS = "revoked";
-        public static final String MODIFIED_TS = "modified";
+        public static final String CLIENT_PKG = "client_pkg";
         public static final String AUTH_INFO_DATA = "auth_info_data";
         
         public static final String[] ALL = {RECIPE_ID, SIGNATURE,
-                                            AUTH_TS, REVOKED_TS, MODIFIED_TS,
-                                            AUTH_INFO_DATA};
+                                            CLIENT_PKG, AUTH_INFO_DATA};
     }
     
     private DatabaseHelper mOpenHelper;
@@ -66,6 +63,8 @@ public class RecipeDbHelper {
     protected RecipeDbHelper(Context c) {
         mOpenHelper = new DatabaseHelper(c);
         database = mOpenHelper.getWritableDatabase();
+        
+        // TOOD: clean the database of old (more than X days) revoked authorizations
     }
     
     /**
@@ -92,7 +91,7 @@ public class RecipeDbHelper {
     protected synchronized boolean storeClientKeyNameEntry(String key, String name) {
         ContentValues cv = new ContentValues(2);
         cv.put(KeysColumns.CLIENT_KEY, key);
-        cv.put(KeysColumns.CLIENT_NAME, name);
+        cv.put(KeysColumns.CLIENT_PKG, name);
         long result;
         try {
             result = database.insertOrThrow(RECIPE_CLIENT_KEYS_TABLE_NAME, null, cv);
@@ -111,32 +110,25 @@ public class RecipeDbHelper {
         return count == 1;
     }
     
-    protected ArrayList<WaveRecipeAuthorization> loadAuthorized(WaveService waveService) {
-        Log.d(TAG, "loadAuthorized(" + waveService + ")");
-        // TODO: optimize query to select by timestamps
+    protected ArrayList<WaveRecipeAuthorization> loadAuthorizations(WaveService waveService) {
+        Log.d(TAG, "loadAuthorizations(" + waveService + ")");
+        
         Cursor c = database.query(RECIPE_AUTH_TABLE_NAME,
                                   AuthColumns.ALL,
                                   null, null, null, null, null);
         
-        ArrayList<WaveRecipeAuthorization> authorized = new ArrayList<WaveRecipeAuthorization>();
+        ArrayList<WaveRecipeAuthorization> authorized = new ArrayList<WaveRecipeAuthorization>(c.getCount());
         if (c.moveToFirst()) {
             for (int i=0; i<c.getCount(); i++) {
-                Timestamp authTime = (c.isNull(2) ? null : Timestamp.valueOf(c.getString(2)));
-                Timestamp revokedTime = (c.isNull(3) ? null : Timestamp.valueOf(c.getString(3)));
-                Timestamp now = new Timestamp(System.currentTimeMillis());
-                if (now.after(authTime)) {
-                    if (revokedTime == null || revokedTime.after(now)) {
-                        String recipeId = c.getString(0);
-                        String authInfoData = c.getString(5);
-                    
-                        try {
-                            WaveRecipe recipe = waveService.getRecipeForId(recipeId);
-                            WaveRecipeAuthorization auth = WaveRecipeAuthorization.fromJSONString(recipe, authInfoData);
-                            authorized.add(auth);
-                        } catch (Exception e) {
-                            Log.w(TAG, "Exception encountered while restoring WaveRecipeAuthorization from SQL database", e);
-                        }
-                    }
+                String recipeId = c.getString(0);
+                // TODO verify the signature column
+                String authInfoData = c.getString(3);
+                try {
+                    WaveRecipe recipe = waveService.getRecipeForId(recipeId);
+                    WaveRecipeAuthorization auth = WaveRecipeAuthorization.fromJSONString(recipe, authInfoData);
+                    authorized.add(auth);
+                } catch (Exception e) {
+                    Log.w(TAG, "Exception encountered while restoring WaveRecipeAuthorization from SQL database", e);
                 }
                 c.moveToNext();
             }
@@ -146,47 +138,57 @@ public class RecipeDbHelper {
         return authorized;
     }
     
-    protected WaveRecipeAuthorization[] loadRevoked() {
-        return null;
-    }
-    
-    protected boolean saveAuthorization(WaveRecipeAuthorization auth) {
-        long row = -1;
-        try {
+    protected boolean insertOrUpdateAuthorization(WaveRecipeAuthorization auth)
+            throws Exception {
+        
+        String recipeId = auth.getRecipe().getId();
+        String clientName = auth.getRecipeClientName().getPackageName();
+        
+        // first check if this authorization already exists in the database
+        String selection = AuthColumns.RECIPE_ID + "=? AND " +
+                           AuthColumns.CLIENT_PKG + "=?";
+        String[] selectionArgs = { recipeId, clientName };
+        Cursor c = database.query(RECIPE_AUTH_TABLE_NAME,
+                                  new String[] { AuthColumns._ID },
+                                  selection, selectionArgs,
+                                  null, null, null);
+        int count = c.getCount();
+        c.close();
+        boolean didSucceed = false;
+        if (count == 0 || count == 1 ) {
             // TODO: make sure the signature from this cert is enough to detect a change in the recipe package
             X509Certificate recipeCertificate = auth.getRecipe().getCertificate();
-        
-            Timestamp authorizedTs = new Timestamp(auth.getAuthorizedDate().getTime());
-            Timestamp modifiedTs = new Timestamp(auth.getModifiedDate().getTime());
-        
+            
             ContentValues cv = new ContentValues(AuthColumns.ALL.length);
             cv.put(AuthColumns.RECIPE_ID, auth.getRecipe().getId());
             cv.put(AuthColumns.SIGNATURE, recipeCertificate.getSignature());
-            cv.put(AuthColumns.AUTH_TS, authorizedTs.toString());
-            cv.putNull(AuthColumns.REVOKED_TS);
-            cv.put(AuthColumns.MODIFIED_TS, modifiedTs.toString());
+            cv.put(AuthColumns.CLIENT_PKG, auth.getRecipeClientName().getPackageName());
             cv.put(AuthColumns.AUTH_INFO_DATA, auth.toJSONString());
-        
-            row = database.insertOrThrow(RECIPE_AUTH_TABLE_NAME, null, cv);
-        } catch (SQLException se) {
-            Log.w(TAG, "SQLException while saving authorization", se);
-            return false;
-        } catch (Exception e) {
-            Log.w(TAG, "Exception while saving authorization", e);
-            return false;
+            
+            try {
+                long row;
+                if (count == 0) {
+                    // not in db, we need to insert
+                    row = database.insertOrThrow(RECIPE_AUTH_TABLE_NAME, null, cv);
+                    didSucceed = (row >= 0);
+                } else {
+                    row = database.update(RECIPE_AUTH_TABLE_NAME, cv,
+                                          selection, selectionArgs);
+                    didSucceed = (row == 1);
+                    if (row != 1) {
+                        throw new Exception("Detected "+row+" rows in db for recipeId="+recipeId+" and clientName="+clientName);
+                    }
+                }
+            } catch (SQLException se) {
+                Log.w(TAG, "SQLException while saving authorization", se);
+                return false;
+            }
+        } else {
+            // matched 2 or more (or less than 0) rows, this is an exception
+            throw new Exception("Detected "+count+" rows in db for recipeId="+recipeId+" and clientName="+clientName);
         }
         
-        return row >= 0;
-    }
-    
-    protected boolean updateAuthorization(WaveRecipeAuthorization auth) {
-        // null implementation
-        return false;
-    }
-    
-    protected boolean revokeAuthorization(WaveRecipeAuthorization auth) {
-        // null implementation
-        return false;
+        return didSucceed;
     }
     
     /**
@@ -212,16 +214,14 @@ public class RecipeDbHelper {
             db.execSQL("CREATE TABLE " + RECIPE_CLIENT_KEYS_TABLE_NAME + " ("
                     + KeysColumns._ID + " INTEGER PRIMARY KEY,"
                     + KeysColumns.CLIENT_KEY + " TEXT UNIQUE NOT NULL,"
-                    + KeysColumns.CLIENT_NAME + " TEXT UNIQUE NOT NULL"
+                    + KeysColumns.CLIENT_PKG + " TEXT UNIQUE NOT NULL"
                     + ");");
             
             db.execSQL("CREATE TABLE " + RECIPE_AUTH_TABLE_NAME + " ("
                     + AuthColumns._ID + " INTEGER PRIMARY KEY,"
-                    + AuthColumns.RECIPE_ID + " TEXT UNIQUE NOT NULL,"
+                    + AuthColumns.RECIPE_ID + " TEXT NOT NULL,"
                     + AuthColumns.SIGNATURE + " BLOB,"
-                    + AuthColumns.AUTH_TS + " TEXT NOT NULL,"
-                    + AuthColumns.REVOKED_TS + " TEXT,"
-                    + AuthColumns.MODIFIED_TS + " TEXT NOT NULL,"
+                    + AuthColumns.CLIENT_PKG + " TEXT NOT NULL,"
                     + AuthColumns.AUTH_INFO_DATA + " TEXT NOT NULL"
                     + ");");
         }

commit 19518f50aafd0151cbcae6ceb417e4b288831b27

Fix for bug introduced in commit 861f4a7dee979174d70b609f004d33dd7a7d56e8
That commit refactored some of the WaveService,
introducing an issue that did not show up until
later.  It seems that methods in the
mPublicBinder IWaveServicePublic.Stub cannot call
methods outside of that stub, possibly only when
such methods return null.

This caused a NullPointerException from
android.os.Parcel.readException(Parcel.java:1253)
when the IPC returns.

src/edu/berkeley/androidwave/waveservice/WaveService.java
@@ -286,39 +286,68 @@ public class WaveService extends Service {
      */
     private final IWaveServicePublic.Stub mPublicBinder = new IWaveServicePublic.Stub() {
         
-        private WaveRecipeAuthorization retrieveAuthorization(String key, String recipeId) {
-            // check the validity of the key
-            if (clientKeyNameMap.containsKey(key)) {
-                // recall the package name for the given key
-                // this is a valid key
-                String packageName = clientKeyNameMap.get(key);
-                WaveRecipeAuthorization auth = getAuthorization(recipeId, packageName);
-                if (auth.validForDate(new Date())) {
-                    return auth;
-                } else {
-                    return null;
-                }
-            }
-            // invalid key, return null
-            Log.d(TAG, "retrieveAuthorization called using invalid key (for recipeId="+recipeId+")");
-            return null;
-        }
-
+        /**
+         * NOTE: These methods have not been refactored, because it is an
+         * apparent limit of AIDL such that we cannot call methods from
+         * outside of this inner class (it might also be possible that we
+         * cannot even call one method from another inside of this Stub()
+         * implementation).
+         */
+        
         /**
          * isAuthorized
          */
         public boolean isAuthorized(String key, String recipeId) {
-            WaveRecipeAuthorization auth = retrieveAuthorization(key, recipeId);
-            return (auth != null);
+            // Log.d(TAG, "mPublicBinder.isAuthorized(\""+key+"\", \""+recipeId+"\")");
+            
+            // check the validity of the key
+            if (clientKeyNameMap.containsKey(key)) {
+                // recall the package name for the given key
+                // this is a valid key
+                String clientPackageName = clientKeyNameMap.get(key);
+                
+                for (WaveRecipeAuthorization auth : authorizations) {
+                    if (auth.getRecipe().getId().equals(recipeId)) {
+                        if (clientPackageName.equals(auth.getRecipeClientName().getPackageName())) {
+                            if (auth.validForDate(new Date())) {
+                                return true;
+                            } else {
+                                Log.d(TAG, "isAuthorized called for revoked recipe (for recipeId="+recipeId+")");
+                            }
+                        }
+                    }
+                }
+            } else {
+                Log.d(TAG, "isAuthorized called using invalid key (for recipeId="+recipeId+")");
+            }
+            return false;
         }
 
         /**
          * retrieveAuthorization
          */
         public WaveRecipeAuthorizationInfo retrieveAuthorizationInfo(String key, String recipeId) {
-            WaveRecipeAuthorization auth = retrieveAuthorization(key, recipeId);
-            if (auth != null) {
-                return auth.asInfo();
+            // Log.d(TAG, "mPublicBinder.retrieveAuthorizationInfo(\""+key+"\", \""+recipeId+"\")");
+
+            // check the validity of the key
+            if (clientKeyNameMap.containsKey(key)) {
+                // recall the package name for the given key
+                // this is a valid key
+                String clientPackageName = clientKeyNameMap.get(key);
+
+                for (WaveRecipeAuthorization auth : authorizations) {
+                    if (auth.getRecipe().getId().equals(recipeId)) {
+                        if (clientPackageName.equals(auth.getRecipeClientName().getPackageName())) {
+                            if (auth.validForDate(new Date())) {
+                                return auth.asInfo();
+                            } else {
+                                Log.d(TAG, "retrieveAuthorizationInfo called for revoked recipe (for recipeId="+recipeId+")");
+                            }
+                        }
+                    }
+                }
+            } else {
+                Log.d(TAG, "retrieveAuthorizationInfo called using invalid key (for recipeId="+recipeId+")");
             }
             return null;
         }
@@ -329,6 +358,8 @@ public class WaveService extends Service {
          * TODO: avoid the use of the key, and determine the caller of the IPC dynamically
          */
         public Intent getAuthorizationIntent(String recipeId, String key) {
+            // Log.d(TAG, "mPublicBinder.getAuthorizationIntent(\""+recipeId+"\", \""+key+"\")");
+
             Intent authIntent = new Intent(ACTION_REQUEST_RECIPE_AUTHORIZE);
             authIntent.putExtra(RECIPE_ID_EXTRA, recipeId);
             authIntent.putExtra(CLIENT_KEY_EXTRA, key);